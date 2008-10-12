7ca0061d49524f72f7a9ab7752110580c0990923
James Bennett <james@b-list.org>
2008-10-12 02:53:27 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='webcolors',
+      version='0.1',
+      description='A library for working with sRGB color specifications as used in HTML and CSS.',
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='http://www.b-list.org/',
+      py_modules=['webcolors'],
+      download_url='http://media.b-list.org/packages/webcolors-0.1.tar.gz',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
