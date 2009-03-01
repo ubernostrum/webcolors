1075e658126c4ff33c525c2f1f896ef4de2aa83e
James Bennett <james@b-list.org>
2009-03-01 01:19:48 -0500

    Because I borked the download URL, let's push it to 1.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='webcolors',
-      version='1.1',
+      version='1.2',
       description='A library for working with sRGB color specifications as used in HTML and CSS.',
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/webcolors/overview/',
       py_modules=['webcolors'],
-      download_url='http://www.bitbucket.org/ubernostrum/webcolors/get/v1.0.gz',
+      download_url='http://www.bitbucket.org/ubernostrum/webcolors/get/v1.2.gz',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
