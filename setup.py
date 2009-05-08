77d03bb6a89f503200ede086e72fca5754cb2f36
James Bennett <james@b-list.org>
2009-05-08 03:40:47 -0400

    Gonna make this document itself.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+import os
+
 from distutils.core import setup
 
 setup(name='webcolors',
       version='1.3',
       description='A library for working with sRGB color specifications as used in HTML and CSS.',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/webcolors/overview/',
