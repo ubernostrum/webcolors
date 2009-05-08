6563e1fcc9542455aac9e159bea330a087000589
James Bennett <james@b-list.org>
2009-05-08 03:37:49 -0400

    Some updates, all to documentation.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='webcolors',
-      version='1.2',
+      version='1.3',
       description='A library for working with sRGB color specifications as used in HTML and CSS.',
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/webcolors/overview/',
       py_modules=['webcolors'],
-      download_url='http://www.bitbucket.org/ubernostrum/webcolors/get/v1.2.gz',
+      download_url='http://bitbucket.org/ubernostrum/webcolors/downloads/webcolors-1.3.tar.gz',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
