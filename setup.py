a3599b24ea245e9511f433d272ef3d353fc710c2
James Bennett <james@b-list.org>
2009-10-24 05:16:18 -0500

    Get ready to bump out a new release.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 from distutils.core import setup
 
 setup(name='webcolors',
-      version='1.3',
+      version='1.3.1',
       description='A library for working with sRGB color specifications as used in HTML and CSS.',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/webcolors/overview/',
       py_modules=['webcolors'],
-      download_url='http://bitbucket.org/ubernostrum/webcolors/downloads/webcolors-1.3.tar.gz',
+      download_url='http://bitbucket.org/ubernostrum/webcolors/downloads/webcolors-1.3.1.tar.gz',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
