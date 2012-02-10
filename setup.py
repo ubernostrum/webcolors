7b121654b23007da51da7ee1869d9c4f92f845e1
James Bennett <james@b-list.org>
2012-02-10 08:01:19 -0600

    Call it 1.4.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 from distutils.core import setup
 
 setup(name='webcolors',
-      version='1.3.1',
+      version='1.4',
       description='A library for working with color names and color value formats defined by the HTML and CSS specifications for use in documents on the Web.',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://www.bitbucket.org/ubernostrum/webcolors/overview/',
       py_modules=['webcolors'],
-      download_url='http://bitbucket.org/ubernostrum/webcolors/downloads/webcolors-1.3.1.tar.gz',
+      download_url='http://bitbucket.org/ubernostrum/webcolors/downloads/webcolors-1.4.tar.gz',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
