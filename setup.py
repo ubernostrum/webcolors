817fb6d66970c27ab99d171fddb8ddc2b17fb8fb
James Bennett <james@b-list.org>
2014-12-11 06:20:35 -0600

    Fix setup.py after rename of README.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='webcolors',
       version='1.5',
       description='A library for working with color names and color value formats defined by the HTML and CSS specifications for use in documents on the Web.',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='https://github.com/ubernostrum/webcolors',
