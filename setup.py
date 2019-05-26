93d23fa5228e7e343108714c44d374d36ea35955
James Bennett <james@b-list.org>
2019-05-25 22:47:06 -0700

    Get ready for 1.9 release.

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires=['six'],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 )
